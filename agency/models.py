d3ae55430a1824d2f00e94ab1e075fae3b38570d
roman <skyfoxwork@gmail.com>
2024-11-30 12:51:56 +0200

    add Topic and Newspaper model to agency models.py

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from core import settings
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    context = models.TextField()
+    published_date = models.DateField()
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="topics"
+    )
+    publishers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.title
