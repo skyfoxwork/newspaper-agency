22b82a03d2eb95e388e5283686d4b363ce8408ca
roman <skyfoxwork@gmail.com>
2024-11-30 12:51:07 +0200

    add Redactor model to accounts models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    def __str__(self):
+        return self.username
