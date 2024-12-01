189e736a401b535120abb72799d5cff6e90606ff
roman <skyfoxwork@gmail.com>
2024-12-01 11:26:24 +0200

    add model Redactor to accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Redactor(AbstractUser):
-    years_of_experience = models.IntegerField()
+    years_of_experience = models.IntegerField(null=True)
 
     def __str__(self):
         return self.username
