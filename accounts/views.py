8f228d7443c7e5ccbfd99ab57411ed20d44f9f1a
roman <skyfoxwork@gmail.com>
2024-12-01 11:25:30 +0200

    add class views  RedactorCrateView, RedactorUpdateView, RedactorDeleteView to accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from accounts.forms import RedactorCreationForm
+from accounts.models import Redactor
+
+
+class RedactorCrateView(generic.CreateView):
+    model = Redactor
+    form_class = RedactorCreationForm
+    success_url = reverse_lazy("agency:redactor-list")
+
+
+class RedactorUpdateView(generic.UpdateView):
+    model = Redactor
+    fields = "__all__"
+    success_url = reverse_lazy("agency:redactor-list")
+
+
+class RedactorDeleteView(generic.DeleteView):
+    model = Redactor
+    success_url = reverse_lazy("agency:redactor-list")
