e39826e46f1a665d0ba1a3878aae7d22666ada7e
roman <skyfoxwork@gmail.com>
2024-12-01 11:23:43 +0200

    add path redactor-create, redactor-update, redactor-delete to accounts/urls.py

---
+++ accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+
+from accounts.views import (
+    RedactorCrateView,
+    RedactorUpdateView,
+    RedactorDeleteView,
+)
+
+urlpatterns = [
+    path("", include("django.contrib.auth.urls")),
+    path("redactor-create/", RedactorCrateView.as_view(), name="redactor-create"),
+    path("redactor-update/<int:pk>/", RedactorUpdateView.as_view(), name="redactor-update"),
+    path("redactor-delete/<int:pk>/", RedactorDeleteView.as_view(), name="redactor-delete"),
+]
+
+app_name = "accounts"
