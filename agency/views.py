08cb770cdd6b6460f2c4c5552cc458250c9415a9
roman <skyfoxwork@gmail.com>
2024-12-01 11:34:00 +0200

    add views index, NewspapersCreateView, NewspapersUpdateView, NewspapersDeleteView, RedactorListView, RedactorDetailView to agency/views.py

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from accounts.models import Redactor
+from agency.models import Newspaper
+
+
+@login_required
+def index(request):
+    newspapers = Newspaper.objects.all()
+    context = {
+        "newspapers": newspapers
+    }
+
+    return render(request, "agency/index.html", context=context)
+
+
+class NewspapersCreateView(generic.CreateView):
+    model = Newspaper
+    fields = "__all__"
+    success_url = reverse_lazy("agency:index")
+
+
+class NewspapersUpdateView(generic.UpdateView):
+    model = Newspaper
+    fields = "__all__"
+    success_url = reverse_lazy("agency:index")
+
+
+class NewspapersDeleteView(generic.DeleteView):
+    model = Newspaper
+    success_url = reverse_lazy("agency:index")
+
+
+class RedactorListView(generic.ListView):
+    model = Redactor
+    template_name = "agency/redactor_list.html"
+
+
+class RedactorDetailView(generic.DetailView):
+    model = Redactor
+    template_name = "agency/redactor_detail.html"
