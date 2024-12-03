879d64927d1b6051ccf920c66bf01078ec37d692
roman <skyfoxwork@gmail.com>
2024-12-03 19:28:44 +0200

    add TopicListView, TopicCreateView, TopicUpdateView, TopicDeleteView views

--- agency/views.py
+++ agency/views.py
@@ -4,11 +4,32 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from accounts.models import Redactor
-from agency.models import Newspaper
+from agency.models import Newspaper, Topic
 
 
 def index(request):
     return render(request, "agency/index.html")
+
+
+class TopicListView(LoginRequiredMixin, generic.ListView):
+    model = Topic
+
+
+class TopicCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Topic
+    fields = ("name",)
+    success_url = reverse_lazy("agency:topic-list")
+
+
+class TopicUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("agency:topic-list")
+
+
+class TopicDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Topic
+    success_url = reverse_lazy("agency:topic-list")
 
 
 class NewspapersListView(LoginRequiredMixin, generic.ListView):
