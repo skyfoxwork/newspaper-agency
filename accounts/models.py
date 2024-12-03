ebec9cd49e33c27814591742b96b7b292a8397ba
roman <skyfoxwork@gmail.com>
2024-12-03 12:17:28 +0200

    fix Redactor model in accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Redactor(AbstractUser):
-    years_of_experience = models.IntegerField(null=True)
+    years_of_experience = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.username
